b6d252747ba7b7ba23205f21ac5f1c8803d55d51
Hudhaifa-1 <aa0411724@gmail.com>
2023-03-14 17:16:10 +0300

    "updated"

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import WelcomeView from '../views/WelcomeView.vue'
+import FinalApp from '../views/FinalApp.vue'
 
 Vue.use(VueRouter)
 
@@ -17,22 +18,16 @@ const routes = [
     component: () => import(/* webpackChunkName: "Login" */ '../views/LoginView.vue')
   },
   {
-    path: '/Home1',
-    name: 'Home',
-    
-    component: () => import(/* webpackChunkName: "Login" */ '../views/HomeView.vue')
-  },
-  {
     path: '/Home',
     name: 'FinalApp',
     
-    component: () => import(/* webpackChunkName: "Login" */ '../views/FinalApp.vue')
+    component: FinalApp
   },
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
   routes
 })
 
